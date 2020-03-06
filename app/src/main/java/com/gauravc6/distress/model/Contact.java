68a73c76f9c8fb3c8d28a93669c646e4c329676b
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2020-03-06 14:00:59 +0530

    add Contact model

---
+++ app/src/main/java/com/gauravc6/distress/model/Contact.java
@@ -0,0 +1,40 @@
+package com.gauravc6.distress.model;
+
+public class Contact {
+    private int id;
+    private String name;
+    private long contactNumber;
+
+    public Contact() {
+    }
+
+    public Contact(int id, String name, long contactNumber) {
+        this.id = id;
+        this.name = name;
+        this.contactNumber = contactNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+}
