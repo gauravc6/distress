24c9e2d60e0848e93e176eef35dafe92739dce47
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2020-03-09 21:25:57 +0530

    Add confirmation functionality to delete contact.

--- app/src/main/java/com/gauravc6/distress/ui/RecyclerViewAdapter.java
+++ app/src/main/java/com/gauravc6/distress/ui/RecyclerViewAdapter.java
@@ -1,6 +1,8 @@
 package com.gauravc6.distress.ui;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +23,9 @@ import java.util.List;
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
     private Context context;
     private List<Contact> contactList;
+    private AlertDialog.Builder builder;
+    private AlertDialog dialog;
+
 
     public RecyclerViewAdapter(Context context, List<Contact> contactList) {
         this.context = context;
@@ -62,6 +67,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
             editButton = contactView.findViewById(R.id.editButton);
             deleteButton = contactView.findViewById(R.id.deleteButton);
+
+            editButton.setOnClickListener(this);
+            deleteButton.setOnClickListener(this);
         }
 
         @Override
@@ -81,11 +89,27 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         }
 
-        private void deleteContact(int id) {
-            DatabaseHandler db = new DatabaseHandler(context);
-            db.deleteContact(id);
-            contactList.remove(getAdapterPosition());
-            notifyItemRemoved(getAdapterPosition());
+        private void deleteContact(final int id) {
+            builder = new AlertDialog.Builder(context);
+            builder.setMessage("Do you want to delete this contact?").setCancelable(true)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            DatabaseHandler db = new DatabaseHandler(context);
+                            db.deleteContact(id);
+                            contactList.remove(getAdapterPosition());
+                            notifyItemRemoved(getAdapterPosition());
+                            dialog.dismiss();
+                        }
+                    }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+            });
+            dialog = builder.create();
+            dialog.setTitle("Delete Contact");
+            dialog.show();
         }
     }
 }
